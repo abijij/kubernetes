007b91d195d96b6d8cc51f8841716c0ecedf4563
abijij <105397465+abijij@users.noreply.github.com>
2024-03-02 22:45:59 -0600

    Se genera la clase Entity de usuarios

---
+++ msvc-usuarios/src/main/java/org/abijij/springcloud/msvc/usuarios/models/entity/Usuario.java
@@ -0,0 +1,73 @@
+package org.abijij.springcloud.msvc.usuarios.models.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usurarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String apellido;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private Long telefono;
+
+    private String password;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Long telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
