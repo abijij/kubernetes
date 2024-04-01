997c55885abbaab1c5b7f8946829b9ba33ba7e54
abijij <105397465+abijij@users.noreply.github.com>
2024-03-31 23:15:17 -0600

    Implementacion de microservicios productos

---
+++ msvc-productos/src/test/java/org/abijij/springcloud/msvc/productos/MsvcProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.abijij.springcloud.msvc.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- msvc-usuarios/src/main/java/org/abijij/springcloud/msvc/usuarios/contollers/UserController.java
+++ msvc-usuarios/src/main/java/org/abijij/springcloud/msvc/usuarios/contollers/UserController.java
@@ -2,16 +2,15 @@ package org.abijij.springcloud.msvc.usuarios.contollers;
 import org.abijij.springcloud.msvc.usuarios.models.entity.Usuario;
 import org.abijij.springcloud.msvc.usuarios.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.print.DocFlavor;
 import java.util.List;
 import java.util.Optional;
 
-@RestController("/api")
+@RestController
 public class UserController {
     @Autowired
     private UsuarioService service;
@@ -29,5 +28,43 @@ public class UserController {
             }
             return ResponseEntity.notFound().build();
     }
+
+//    @PostMapping("/CreateUser")
+//    @ResponseStatus(HttpStatus.CREATED)
+//    public Usuario create(@RequestBody  Usuario usuario){
+//        return service.save(usuario);
+//    }
+
+    @PostMapping("/CreateUser")
+    public ResponseEntity<?> create(@RequestBody  Usuario usuario){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(usuario));
+    }
+
+    @PutMapping("/UpdateById/{id}")
+    public ResponseEntity<?> update(@RequestBody Usuario usuario, @PathVariable Long id){
+        Optional<Usuario> o = service.findById(id);
+        if (o.isPresent()){
+            Usuario usuarioDb = o.get();
+            usuarioDb.setNombre(usuario.getNombre());
+            usuarioDb.setApellido(usuario.getApellido());
+            usuarioDb.setTelefono(usuario.getTelefono());
+            usuarioDb.setEmail(usuario.getEmail());
+            usuarioDb.setPassword(usuario.getPassword());
+            return  ResponseEntity.status(HttpStatus.CREATED).body(service.save(usuarioDb));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/delteById/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        Optional<Usuario> o = service.findById(id);
+        if (o.isPresent()){
+            service.delete(id);
+            return ResponseEntity.noContent().build();
+        }
+        return  ResponseEntity.notFound().build();
+    }
+
+
 }
 
