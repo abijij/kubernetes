076a59a4cb6f37aa75953aac5ff58a4bef5da6c7
abijij <105397465+abijij@users.noreply.github.com>
2024-04-13 21:36:24 -0600

    Implementacion de msvc Factura

---
+++ msvc-factura/src/main/java/org/abijij/springcloud/msvc/factura/msvcfactura/MsvcFacturaApplication.java
@@ -0,0 +1,13 @@
+package org.abijij.springcloud.msvc.factura.msvcfactura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcFacturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcFacturaApplication.class, args);
+	}
+
+}
---
+++ msvc-factura/src/test/java/org/abijij/springcloud/msvc/factura/msvcfactura/MsvcFacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.abijij.springcloud.msvc.factura.msvcfactura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcFacturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
